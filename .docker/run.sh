ff1f73f2707e34b10593b907115e99d6dd25f7c4
Sylvain Corsini <sylvain.corsini@protonmail.com>
2023-08-09 20:09:01 +0200

    fix: invalid litestream restore cmd

--- .docker/run.sh
+++ .docker/run.sh
@@ -5,7 +5,7 @@ if [ -f /data/data.db ]; then
 	echo "Database already exists, skipping restore"
 else
 	echo "No database found, restoring from replica if exists"
-	litestream restore -v -if-replica-exists -o /data/data.db "${REPLICA_URL}"
+	litestream restore -v -if-replica-exists /data/data.db
 fi
 
 exec litestream replicate -exec "/usr/local/bin/lets-school-central-backend serve --http=0.0.0.0:${PORT} --dir=/data"
